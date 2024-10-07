fb50ff134d5bda463ef800d10781ed3ea09944fa
dglt1 <mwf4321@outlook.com>
2024-10-07 12:13:28 -0400

    added launch script

---
+++ launch.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Load Cargo environment
+. "$HOME/.cargo/env"
+
+# Environment variables
+export IDENTITY_KEYPAIR_FILE=/home/solana/atlasmod/atlas-keypair.json
+export GRPC_URL=http://localhost:10001
+export RPC_URL=http://localhost
+export PORT=4040
+export TPU_CONNECTION_POOL_SIZE=1
+#export X_TOKEN=
+export TXN_SENDER_THREADS=1500
+export MAX_TXN_SEND_RETRIES=3
+export TXN_SEND_RETRY_INTERVAL=0.4
+export MAX_RETRY_QUEUE_SIZE=4000
+
+# Logging and metrics
+export RUST_LOG=info
+export METRICS_URI=localhost
+export METRICS_PORT=7998
+
+# Run the application
+cargo run --release
